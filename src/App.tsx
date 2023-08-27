701c5036113ed1a5669f4575281a43b28391ee4b
Rami Lorca <ramilorca@Ramis-MBP.cable.rcn.com>
2023-08-26 21:04:49 -0400

    added App styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import Button from "./components/Button";
-import Alert from "./components/Alert";
-import { useState } from "react";
+// import Button from "./components/Button";
+// import Alert from "./components/Alert";
+// import { useState } from "react";
 import { addToCart, Product } from "./shoppingSlice";
 import { useDispatch, useSelector } from "react-redux";
 import type { RootState } from './store';
@@ -9,19 +9,20 @@ import Profile from "./components/Profile";
 import ChangeColor from "./components/ChangeColor";
 import DisplayUserInfo from "./components/DisplayUserInfo";
 import IdSlider from "./components/IdSlider";
+import './App.scss'
 
 const App: React.FC = () => {
 
   const dispatch = useDispatch();
   const shopping = useSelector((state: RootState) => state.shopping);
 
-  const [alertVisible, setAlertVisibility] = useState(false);
+  // const [alertVisible, setAlertVisibility] = useState(false);
 
   return (
-    <div>
+    <div className="app-container">
 
-      { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert> }
-      <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
+      {/* { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert> }
+      <Button onClick={() => setAlertVisibility(true)}>My Button</Button> */}
 
       {shopping.products.map((product: Product) => (
         <div key={product.id}>
